7ea138f847fedff1b2a45b976c1898bf7fe0fce9
DevRamona <r.ingabire@alustudent.com>
2024-06-14 11:51:31 +0200

    a fetching posts and comments using async and await

---
+++ fetchposts.js
@@ -0,0 +1,28 @@
+const good = document.getElementById('container')
+
+function createContainer(heading, paragraph, comment) {
+    const parent = document.createElement('div');
+    const title = document.createElement('h1');
+    const para = document.createElement('p');
+    const list = document.createElement('li');
+    title.innerText = heading.toUpperCase();
+    para.innerText = paragraph;
+    list.innerText = comment;
+    parent.appendChild(title);
+    parent.appendChild(para);
+    parent.appendChild(list);
+    container.appendChild(parent);
+    document.body.appendChild(container)
+}
+
+async function fetchPosts() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const info = await response.json()
+
+    for(let i = 0; i<=10; i++) {
+        const response2 = await fetch(`https://jsonplaceholder.typicode.com/comments/${info[i]["id"]}`);
+            const data2 = await response2.json()
+            createContainer(info[i]["title"], info[i]["body"], data2.body)
+    }
+}
+fetchPosts();
