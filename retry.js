41a4262887f29df36b1c46eb88406c65f493debe
DevRamona <r.ingabire@alustudent.com>
2024-07-01 12:34:58 +0200

    How to do retries in js

--- retry.js
+++ retry.js
@@ -1,15 +1,33 @@
-const url = `https://jsonplaceholder.typicode.com/users`
-const retryAsync = (url, maxRetries = 5) {
+const url = `https://jsonplaceholder.typicode.com/users/`
+const retryAsync = async(url, maxRetries = 5) =>  {
     try {
         let retries = 0;
     if(retries < maxRetries) {
         const data = await fetch(url)
+        const response = await data.json();
+        return response;
 
     }
 
     } catch(error) {
-        console.error("Can't fetch")
+        if(retries === maxRetries){
+            console.error("Can't fetch, retries have exceeded")
+            retries ++;
+
+        }
+        
     }
     
 
 }
+retryAsync(url, 3)
+.then((data) => {
+    if(data) {
+        console.log(data)
+    } else {
+        console.log("Retries exceeded")
+    }
+})
+.catch((error) => {
+    console.log("Error logged")
+})
